2d94300eb735fe2ad962ae9107059e681b3bcf0f
Anh Vu <anhvungoc.21@gmail.com>
2023-04-10 02:58:00 -0500

    Added User and Chat models. Defined basic user-related queries.

---
+++ fetch/Models/Chat.swift
@@ -0,0 +1,36 @@
+//
+//  ChatRoom.swift
+//  fetch
+//
+//  Created by Anh Vu on 4/10/23.
+//
+
+import Foundation
+import ParseSwift
+
+struct ChatRoom: ParseObject {
+    // These are required by `ParseObject`.
+    var objectId: String?
+    var createdAt: Date?
+    var updatedAt: Date?
+    var ACL: ParseACL?
+    var originalData: Data?
+    
+    // Chat room properties
+    var messages: [ChatMessage]?
+}
+
+struct ChatMessage: ParseObject {
+    // These are required by `ParseObject`.
+    var objectId: String?
+    var createdAt: Date?
+    var updatedAt: Date?
+    var ACL: ParseACL?
+    var originalData: Data?
+    
+    // Chat message properties
+    var user: String?
+    var message: String?
+}
+
+
--- fetch/Models/User.swift
+++ fetch/Models/User.swift
@@ -6,13 +6,11 @@
 //
 
 import Foundation
-
-// TODO: Pt 1 - Import Parse Swift
 import ParseSwift
+import CoreLocation
+import UIKit
 
-// TODO: Pt 1 - Create Parse User model
-// https://github.com/parse-community/Parse-Swift/blob/3d4bb13acd7496a49b259e541928ad493219d363/ParseSwift.playground/Pages/3%20-%20User%20-%20Sign%20Up.xcplaygroundpage/Contents.swift#L16
-
+// A User also represents a Dog
 struct User: ParseUser {
     // These are required by `ParseObject`.
     var objectId: String?
@@ -20,16 +18,142 @@ struct User: ParseUser {
     var updatedAt: Date?
     var ACL: ParseACL?
     var originalData: Data?
-
+    
     // These are required by `ParseUser`.
     var username: String?
     var email: String?
     var emailVerified: Bool?
     var password: String?
     var authData: [String: [String: String]?]?
-
-    // Your custom properties.
-
-    // TODO: Pt 2 - Add custom property for `lastPostedDate`
-    var lastPostedDate: Date?
+    
+    // Custom Properties for Fetch
+    // -- Dog traits --
+    var name: String?
+    var breed: String?
+    var age: Int?
+    var bio: String?
+    var preferences: [String]?
+    var images: [ParseFile]?
+    
+    // -- User metadata --
+    var location: ParseGeoPoint?
+    var recentlyActive: Bool?
+    
+    // -- Cross-user data  --
+    // Strings represent usernames
+    var dislikedUsers: [String]?
+    var likedUsers: [String]? // Users that self liked
+    var usersLiked: [String]? // Users that liked self
+    var matchedUsers: [String]?
 }
+
+
+// For display in My Profile view
+// MARK: The current plan is NOT to fetch all parts of profile upon login.
+func getMyProfile() {
+    guard let myUser = User.current else {
+        print("Failed to get current user.")
+        return
+    }
+    
+    let constraint = "username" == myUser.username
+    let query = User.query(constraint)
+    
+    query.find { result in
+        // ... Store data of own user
+    }
+}
+
+
+// To be defined in Feed view
+// MARK: Is there a good way to specify constraints for the fetch, or do we have to pull everything and then filter?
+// MARK: Maybe implement sorting profiles based on matching preferences
+// Back4App Query Codebook: Queries with constraints involving array type fields?
+let DEFAULT_MILE_DIAMETER: Double = 5
+func getProfiles() {
+    guard let myUser = User.current else {
+        print("Failed to get current user.")
+        return
+    }
+    
+    // TODO: Define constraints based on LOCATION. Probably hard code a "diameter" at first.
+    guard let location = myUser.location,
+          let geopoint = try? ParseGeoPoint(latitude: location.latitude,
+                                            longitude: location.longitude) else {
+        print("Failed to get current user's location")
+        return
+    }
+   
+    let constraint = withinKilometers(key: "location", geoPoint: geopoint, distance: DEFAULT_MILE_DIAMETER)
+    let query = User.query(constraint).include("user")
+
+    query.find { result in // [weak self]
+        switch result {
+        case .success(let posts):
+            // Update the local posts property with fetched posts
+            print(posts) // for now, to avoid type-checking warning message
+            
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    }
+}
+
+
+// To be declared in Signup view
+// MARK: Handle login/logout with notification center
+func signup(username: String, email: String, password: String) {
+    // Create new user
+    var newUser = User()
+    newUser.username = username
+    newUser.email = email
+    newUser.password = password
+
+    newUser.signup { result in // [weak self]
+        switch result {
+        case .success(let user):
+            // Register login event
+            NotificationCenter.default.post(name: Notification.Name("login"), object: nil)
+            print(user) // for now, to avoid type-checking warning message
+
+        case .failure(let error):
+            // TODO: Create & display error alert
+            print(error.localizedDescription)
+        }
+    }
+}
+
+
+// To be declared in Login view
+// MARK: Handle login/logout with notification center
+func login(username: String, password: String) {
+    User.login(username: username, password: password) { result in // [weak self]
+        switch result {
+        case .success(let user):            
+            NotificationCenter.default.post(name: Notification.Name("login"), object: nil)
+            print(user) // for now, to avoid type-checking warning message
+
+        case .failure(let error):
+            // TODO: Create & Display error alert
+            print(error.localizedDescription)
+        }
+    }
+}
+
+
+// To be declared in MyProfile view
+// MARK: Handle login/logout with notification center
+func logout() {
+    // TODO: Create alert
+    let alertController = UIAlertController(title: "", message: "", preferredStyle: .alert)
+    let logOutAction = UIAlertAction(title: "Log out", style: .destructive) { _ in
+        NotificationCenter.default.post(name: Notification.Name("logout"), object: nil)
+    }
+    let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+    
+    // TODO: Present alert
+    alertController.addAction(logOutAction)
+    alertController.addAction(cancelAction)
+//    present(alertController, animated: true)
+}
+
